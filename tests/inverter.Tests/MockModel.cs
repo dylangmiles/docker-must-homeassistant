7d700d75e03acd1e8b6d616a2dd5100fdd97aa83
Dylan Miles <dylan@alchemysoftware.co.za>
2020-10-22 21:27:26 +0200

    Modbus message to model

--- tests/inverter.Tests/MockModel.cs
+++ tests/inverter.Tests/MockModel.cs
@@ -10,5 +10,11 @@ namespace inverter.Tests
 
         [ModbusSensor(20000, "sensor1", 1.0)]
         public short? Sensor1 { get; internal set; }
+
+        [ModbusSensor(20001, "sensor2", 1.0)]
+        public object Sensor2 { get; internal set; }
+
+        [ModbusSensor(20002, "sensor3", 1.0)]
+        public short Sensor3 { get; internal set; }
     }
 }
--- tests/inverter.Tests/ModbusToModelMapperTest.cs
+++ tests/inverter.Tests/ModbusToModelMapperTest.cs
@@ -19,6 +19,23 @@ namespace inverter.Tests
             ModbusToModelMapper.Map(startAddress, values, model);
 
             Assert.AreEqual(model.Sensor1, (short)1);
+            Assert.AreEqual(model.Sensor2, null);
+
+        }
+
+        [TestMethod]
+        public void Map_ThreeShorts_Success()
+        {
+            //Given 
+            var startAddress = (short)20000;
+            var values = new short[] {1, 2, 3};
+            var model = new MockModel();
+
+            ModbusToModelMapper.Map(startAddress, values, model);
+
+            Assert.AreEqual(model.Sensor1, (short)1);
+            Assert.AreEqual(model.Sensor2, (short)2);
+            Assert.AreEqual(model.Sensor3, (short)3);
 
         }
     }
