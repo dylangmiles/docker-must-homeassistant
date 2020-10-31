e9f9d23d8fafc4bca0908559deb5f6b2d16e6bc9
Dylan Miles <dylan@alchemysoftware.co.za>
2020-10-31 20:25:58 +0200

    Update entrypoint and fix paths

--- src/inverter/inverter-mqtt/entrypoint.sh
+++ src/inverter/inverter-mqtt/entrypoint.sh
@@ -5,10 +5,10 @@ export TERM=xterm
 # Init the mqtt server for the first time, then every 5 minutes
 # This will re-create the auto-created topics in the MQTT server if HA is restarted...
 
-watch -n 300 /app/mqtt-init.sh > /dev/null 2>&1 &
+watch -n 300 /app/inverter-mqtt/mqtt-init.sh > /dev/null 2>&1 &
 
 # Run the MQTT Subscriber process in the background (so that way we can change the configuration on the inverter from home assistant)
 #/app/mqtt-subscriber.sh &
 
 # execute exactly every 30 seconds...
-watch -n 30 /app/mqtt-push.sh > /dev/null 2>&1
+watch -n 30 /app/inverter-mqtt/mqtt-push.sh > /dev/null 2>&1
