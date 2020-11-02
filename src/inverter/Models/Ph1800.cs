c06e684aa311ce214c46b82cd459f9b13f841ea0
Dylan Miles <dylan@alchemysoftware.co.za>
2020-11-03 00:28:35 +0200

    Trying to get calculation for accumlated values working when there is a high.

--- src/inverter/Models/Ph1800.cs
+++ src/inverter/Models/Ph1800.cs
@@ -42,7 +42,29 @@ namespace inverter.Models
         public double? AccumulatedLoadPower
         {   get
             {
-                return (this.AccumulatedLoadPowerH ?? 0d) * 1000d + (this.AccumulatedLoadPowerL ?? 0d) * 0.1d;
+
+                var nullable1 = this.AccumulatedLoadPowerH;
+                double? nullable8;
+                if (nullable1.HasValue)
+                {
+                    nullable1 = this.AccumulatedLoadPowerH;
+                    int? nullable4 = nullable1.HasValue ? new int?((int) nullable1.GetValueOrDefault() * 1000) : new int?();
+                    nullable1 = this.AccumulatedLoadPowerL;
+                    var nullable2 = nullable1.HasValue ? new int?((int) nullable1.GetValueOrDefault()) : new int?();
+                    if ((nullable4.HasValue & nullable2.HasValue ? new int?(nullable4.GetValueOrDefault() + nullable2.GetValueOrDefault()) : new int?()).HasValue)
+                    {
+                        nullable1 = this.AccumulatedLoadPowerL;
+                        nullable8 = nullable1.HasValue ? new double?((double) nullable1.GetValueOrDefault() * 0.1) : new double?();
+                    }
+                    else
+                        nullable8 = new double?(0.0);
+                    }
+                else
+                    nullable8 = new double?(0.0);
+                
+                return nullable8;
+
+                //return (this.AccumulatedLoadPowerH ?? 0d) * 1000d + (this.AccumulatedLoadPowerL ?? 0d) * 0.1d;
             }
         }
 
