b579198432bf111b197fd9057056155b1ec16223
Dylan Miles <dylan@alchemysoftware.co.za>
2020-11-07 22:24:14 +0200

    Modbus writer is started.

---
+++ src/inverter/Modbus/ModbusWriter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+
+namespace inverter.Modbus
+{
+    public class ModbusWriter
+    {
+        private ISerialPort _serialPort;
+
+        public ModbusWriter(ISerialPort serialPort)
+        {
+            _serialPort = serialPort;
+        }
+
+        public void write(byte deviceId, ushort address, ushort[] values)
+        {
+            //Construct the request
+            var requestLength = 9 + 2 * values.Length;
+            var request = new byte[requestLength];
+            using (var stream = new MemoryStream(request))
+            using (var writer = new BigEndianBinaryWriter(stream))
+            {
+                writer.Write(deviceId);                     // DeviceId
+                writer.Write(0x10);                         // Write multiple registers
+                writer.Write(address);                      // Start address
+                writer.Write((ushort)values.Length);        // Number of values to write
+                writer.Write((byte)(values.Length * 2));    // Number of values to write
+
+                foreach( var value in values)
+                {
+                    writer.Write(value);
+                }
+
+                writer.Write(0x00);                         // CRC
+                writer.Write(0x00);                         // CRC
+            }
+
+            //Calculate and write the CRC
+            var crc = Utils.CalculateCrc(request, 0, requestLength - 2);
+
+            using (var stream = new MemoryStream(request, requestLength - 2, 2))
+            using (var writer = new BigEndianBinaryWriter(stream))
+            {
+                writer.Write(crc);
+            }
+
+            //Send the message over the wire
+            _serialPort.Write(request, 0, request.Length);
+
+
+            //Read the response
+            var response = new byte[8];
+            var read = _serialPort.Read(response, 0, response.Length);
+
+            //Check read length
+            //if (read != response.Length)
+            //{
+            //    throw new InvalidDataException($"Invalid length of data read. Expected {response.Length} bytes and got {read} bytes.");
+            //}
+
+
+            using (var stream = new MemoryStream(response))
+            using (var reader = new BigEndianBinaryReader(stream))
+            {
+                var returnDeviceId = reader.ReadByte();
+                //if (returnDeviceId != deviceId) throw new InvalidDataException($"Invalid Device Id. Expected {deviceId} and got {returnDeviceId}");
+
+                var returnFunctionCode = reader.ReadByte();
+                //if (returnFunctionCode != 0x10) throw new InvalidDataException($"Invalid Function Code. Expected 0x10 and got {returnFunctionCode}.");
+
+                var returnSensorCount = reader.ReadUInt6();
+                //if (returnSensorCount != values.Length) throw new InvalidDataException($"Invalid Sensor Count. Expected {values.Length} bytes and received {returnSensorCount}.");
+
+                var returnCrc = reader.ReadUInt6();
+                var calcCrc = Utils.CalculateCrc(response, 0, response.Length - 2);
+
+                //if (returnCrc != calcCrc)
+                //{
+                //    throw new InvalidDataException($"Invalid CRC. Expected {calcCrc} and got {returnCrc}.");
+                //}
+            }
+
+        }
+    }
+}
---
+++ tests/inverter.Tests/Modbus/ModbusWriterTest.cs
@@ -0,0 +1,87 @@
+﻿using inverter.Modbus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace inverter.Tests.Modbus
+{
+    [TestClass]
+    public class ModbusWriterTest
+    {
+
+        //Windows   - Little Endian 0x01 0x00
+        //Linux     - Big Endian    0x00 0x01
+        //Modbus    - Big Endian    0x00 0x01
+
+        [TestMethod]
+        public void Write_OneValue_Success()
+        {
+            var port = new Mock<ISerialPort>();
+            
+            byte[] request = null;
+            port.Setup(foo => foo.Write(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>())).Callback<byte[],int,int>((b,o,c) => request = b);
+
+            byte[] response = null;
+            port.Setup(foo => foo.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>())).Callback<byte[], int, int>((b,o,c) => {
+
+                using (var stream = new MemoryStream(b))
+                using( var writer = new BigEndianBinaryWriter(stream))
+                {
+                    writer.Write((byte)0x04);   // Device Id
+                    writer.Write((byte)0x10);   // Function - 16 - write multiple registers
+                    writer.Write((byte)0x4e);   // Address 20109
+                    writer.Write((byte)0x8d);   // Address 20109
+                    writer.Write((byte)0x00);   // Number of registers written
+                    writer.Write((byte)0x01);   // Number of registers written
+                    
+                    writer.Write((byte)0x86);   //CRC
+                    writer.Write((byte)0x9f);   //CRC
+                }
+
+                response = b;
+
+            }).Returns(7);
+
+
+             var writer = new ModbusWriter(port.Object);
+
+
+            byte deviceId = 4;
+            ushort address = 20109;
+            ushort[] values = {3};
+
+
+            writer.write(deviceId, address, values);
+
+
+            /* Energy use lookup
+            "",
+            "SBU",
+            "SUB",
+            "UTI",
+            "SOL"
+            */
+            //Message to set 20109 to 3 ... Set EnergyUse to Utility.
+            //04    (4)             device id
+            //10    (16)            function id
+            //4e8d  (20109)         starting address
+            //00    (1)             number of registers to write
+            //01    (1)             number of registers to write
+            //02    (2)             number of bytes to write
+            //0003  (3)             value of address
+            //69d8                  crc
+            CollectionAssert.AreEqual(new byte[] { 0x04, 0x10, 0x4e, 0x8d, 0x00, 0x01, 0x02, 0x00, 0x03, 0x69, 0xd8 }, request);
+
+            //Response acknowledgement
+            //04    (4)             device id
+            //10    (16)            function id
+            //4e8d  (20109)         the starting address
+            //0001  (1)             number of registers written to
+            //869f                  crc
+            CollectionAssert.AreEqual(new byte[] { 0x04, 0x10, 0x4e, 0x8d, 0x00, 0x01, 0x86, 0x9f }, response);
+
+        }
+    }
+}
